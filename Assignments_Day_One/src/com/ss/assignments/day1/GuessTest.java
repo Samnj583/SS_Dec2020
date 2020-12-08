949cd356dbf1bc384e249e2d017aba155493088e
Samuel Johnson <samuel.johnson@smoothstack.com>
2020-12-08 01:13:09 -0700

    First iteration of Day One's assignments
    Wrote out and tested code, needs some comments.

---
+++ Assignments_Day_One/src/com/ss/assignments/day1/GuessTest.java
@@ -0,0 +1,50 @@
+/**
+ *  This is a program that lets the user guess a number randomly chosen
+ */
+package com.ss.assignments.day1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Samuel Johnson
+ *
+ */
+public class GuessTest {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int ans = rand.nextInt(100)+1;
+		int guesses = 5;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Guess the number between 1 and 100");
+		
+		int userGuessInt;
+		
+		while(guesses > 0) {
+			String userGuessStr = scan.nextLine();
+			try {
+			userGuessInt = Integer.parseInt(userGuessStr);
+			}
+			catch(Exception e){
+				System.out.println("Integers only");
+				continue;
+			}
+			if(userGuessInt > 100 || userGuessInt < 0) {
+				System.out.println("Remember it is between 1 and 100. Try again");
+			}
+			if((ans+10) >= userGuessInt && userGuessInt >= (ans-10)) {
+				System.out.println("Good, the answer is "+ans);
+				break;
+			}
+			else {
+				guesses--;
+				if(guesses == 0) {
+					System.out.println("Sorry the answer is "+ans);
+					break;
+				}
+				System.out.println("Try again");
+			}
+		}
+	}
+}
---
+++ Assignments_Day_One/src/com/ss/assignments/day1/PrintTest.java
@@ -0,0 +1,47 @@
+/**
+ *  This is a program that test my ability to print
+ */
+package com.ss.assignments.day1;
+
+/**
+ * @author Samuel Johnson
+ *
+ */
+public class PrintTest {
+	public static void main(String[] args) {
+		System.out.println("1)");
+		System.out.println("*        ");
+		System.out.println("**       ");
+		System.out.println("***      ");
+		System.out.println("****     ");
+		System.out.println(".........");
+		
+		System.out.println("");
+		
+		System.out.println("2)");
+		System.out.println("..........");
+		System.out.println("****      ");
+		System.out.println("***       ");
+		System.out.println("**        ");
+		System.out.println("*         ");
+		
+		System.out.println("");
+		
+		System.out.println("3)");
+		System.out.println("     *     ");
+		System.out.println("    ***    ");
+		System.out.println("   *****   ");
+		System.out.println("  *******  ");
+		System.out.println("...........");
+		
+		System.out.println("");
+		
+		System.out.println("4)");
+		System.out.println("............");
+		System.out.println("  *******");
+		System.out.println("   *****");
+		System.out.println("    ***");
+		System.out.println("     *");
+		
+	}
+}
